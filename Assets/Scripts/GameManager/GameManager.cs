e75f930bbedf8f006d5a44f6981fdbca1588a7e8
Kacper <kacper2837@gmail.com>
2025-06-17 14:32:19 +0200

    Added UI for Title screen and Game Over screen

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject startMenu;
+    public GameObject gameOverMenu;
+    public GameObject player;
+
+    private bool isGameOver = false;
+
+    void Start()
+    {
+        Time.timeScale = 0f; // Pause game initially
+        startMenu.SetActive(true);
+        gameOverMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        startMenu.SetActive(false);
+        Time.timeScale = 1f; // Unpause game
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver) return;
+        isGameOver = true;
+
+        Time.timeScale = 0f; // Pause game
+        gameOverMenu.SetActive(true);
+    }
+    public void ReturnToMainMenu()
+    {
+        startMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload scene
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
